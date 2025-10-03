5d9b2515ee62d28aa0ff099ef1165d506d407da4
Third <rafi.34hack@gmail.com>
2025-10-03 17:08:52 +0700

    feat: added pasca-praktikum 4

---
+++ Java_OOP/Praktikum 4/prak/Attack/Attack.java
@@ -0,0 +1,9 @@
+public interface Attack {
+    public void performAttack();
+
+    public default void showAttackInfo() {
+        System.out.println("Karakter ini bisa melakukan serangan!");
+        // instance of
+    }
+
+}
