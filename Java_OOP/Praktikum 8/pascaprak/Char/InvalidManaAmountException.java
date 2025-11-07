166d550e309e8cb9f9f0c0c1100113e36c1bd495
Third <rafi.34hack@gmail.com>
2025-11-07 16:48:21 +0700

    chore: moved files and added pasca praktikum

---
+++ Java_OOP/Praktikum 8/pascaprak/Char/InvalidManaAmountException.java
@@ -0,0 +1,16 @@
+class InvalidManaAmountException extends Exception {
+    // Exception yang di-throw ketika nilai mana yang diberikan tidak valid (negatif atau nol)
+    private int manaAmount;
+
+    public InvalidManaAmountException(int manaAmount) {
+        super("Jumlah mana tidak valid: " + manaAmount);
+        this.manaAmount = manaAmount;
+        
+        // Todo: Constructor dengan message: "Jumlah mana tidak valid: <manaAmount>"
+        // lalu inisialisasi manaAmount
+    }
+
+    public int getManaAmount() {
+        return manaAmount;
+    }
+}
